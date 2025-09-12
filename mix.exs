2631121aee4cebe85a25d60bef2ffdeba2d887a1
Nathan <nathan.han@epitech.eu>
2025-09-12 15:14:57 +0200

    test CI

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule WhisprNotification.MixProject do
     [
       app: :whispr_notification,
       version: "0.1.0",
-      elixir: "~> 1.15",
+      elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
