faef3ed8e8e89f9e9e7fce81d57ccefb9b52bfa9
Nathan Han <nathan.han@epitech.eu>
2026-04-03 11:01:49 +0200

    fix: test

--- lib/whispr_notifications/auth/jwks_cache.ex
+++ lib/whispr_notifications/auth/jwks_cache.ex
@@ -5,12 +5,14 @@ defmodule WhisprNotifications.Auth.JwksCache do
   alias WhisprNotifications.Auth.Jwks
 
   def start_link(opts) do
-    GenServer.start_link(__MODULE__, opts, name: __MODULE__)
+    {name, opts} = Keyword.pop(opts, :name, __MODULE__)
+    GenServer.start_link(__MODULE__, opts, name: name)
   end
 
-  @spec get_key(String.t()) :: {:ok, JOSE.JWK.t()} | :error
-  def get_key(kid) when is_binary(kid) do
-    GenServer.call(__MODULE__, {:get_key, kid})
+  @spec get_key(String.t(), GenServer.server()) ::
+          {:ok, JOSE.JWK.t()} | {:error, :unknown_kid}
+  def get_key(kid, server \\ __MODULE__) when is_binary(kid) do
+    GenServer.call(server, {:get_key, kid})
   end
 
   @doc false
@@ -21,24 +23,57 @@ defmodule WhisprNotifications.Auth.JwksCache do
 
   @impl true
   def init(opts) do
+    refresh_ms = Keyword.get(opts, :refresh_interval_ms)
+    jwks_url = resolve_jwks_url(opts)
+    http_get_fun = Keyword.get(opts, :http_get_fun)
+
+    case load_keys(opts, jwks_url, http_get_fun) do
+      {:ok, keys} ->
+        state = %{keys: keys, refresh_interval_ms: refresh_ms, jwks_url: jwks_url, http_get_fun: http_get_fun}
+        {:ok, state}
+
+      {:error, reason} ->
+        {:stop, reason}
+    end
+  end
+
+  defp resolve_jwks_url(opts) do
+    Keyword.get(opts, :jwks_url) ||
+      Keyword.get(opts, :url) ||
+      get_in(Application.get_env(:whispr_notification, :jwt, []), [:jwks_url])
+  end
+
+  defp load_keys(opts, jwks_url, http_get_fun) do
     cond do
       json = opts[:inline_jwks] ->
-        case Jwks.keys_from_json(json) do
-          {:ok, keys} -> {:ok, %{keys: keys}}
-          {:error, reason} -> {:stop, reason}
+        Jwks.keys_from_json(json)
+
+      is_function(http_get_fun, 1) ->
+        url = jwks_url || ""
+
+        case http_get_fun.(url) do
+          {:ok, %{status: 200, body: body}} ->
+            body_json = if is_binary(body), do: body, else: Jason.encode!(body)
+            Jwks.keys_from_json(body_json)
+
+          {:ok, %{status: status}} ->
+            {:error, {:http, status}}
+
+          {:error, reason} ->
+            {:error, reason}
+
+          other ->
+            {:error, {:bad_http_result, other}}
         end
 
-      url = opts[:url] ->
-        case Jwks.fetch_keys(url) do
-          {:ok, keys} -> {:ok, %{keys: keys}}
-          {:error, reason} -> {:stop, reason}
-        end
+      is_binary(jwks_url) and jwks_url != "" ->
+        Jwks.fetch_keys(jwks_url)
 
       opts[:allow_empty] == true ->
-        {:ok, %{keys: %{}}}
+        {:ok, %{}}
 
       true ->
-        {:stop, {:bad_jwks_opts, opts}}
+        {:error, {:bad_jwks_opts, opts}}
     end
   end
 
@@ -47,7 +82,7 @@ defmodule WhisprNotifications.Auth.JwksCache do
     reply =
       case Map.get(keys, kid) do
         %JOSE.JWK{} = jwk -> {:ok, jwk}
-        _ -> :error
+        _ -> {:error, :unknown_kid}
       end
 
     {:reply, reply, state}
